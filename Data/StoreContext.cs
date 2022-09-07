2f8a7f1a2df8c794e91d20b50c6e9e68c336914a
danny-pye <danny.luke.pye@gmail.com>
2022-09-07 20:58:59 +0100

    first one

---
+++ Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+public class StoreContext : DbContext
+{
+    //constructor
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    // products is the name of the table in the database
+    public DbSet<Product> Products { get; set; }
+}
